d25b7880d098b700397afacca8a89fafc6361a5f
Ranjinyu Tian <397026784@qq.com>
2020-02-08 20:26:23 +0000

    chapter 4

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from rango import views
 from django.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', views.index, name='index'),
+
     path('rango/',include('rango.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
