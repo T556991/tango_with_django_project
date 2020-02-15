c94a7fe092d37163703cc149abe995b7a0f97f7c
Ranjinyu Tian <397026784@qq.com>
2020-02-15 13:20:32 +0000

    chapter8(1)

--- rango/views.py
+++ rango/views.py
@@ -19,7 +19,7 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     
-    return render(request,'rango/about.html')
+    return render(request,'rango/about.html',{})
 
 def show_category(request, category_name_slug):
 
@@ -50,7 +50,7 @@ def add_category(request):
 
             form.save(commit=True)
 
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
 
 
         else:
@@ -67,7 +67,7 @@ def add_page(request, category_name_slug):
     
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
