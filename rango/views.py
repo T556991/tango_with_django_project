853e7e9f9843e6d6f035a58ed148e090e7169863
Ranjinyu Tian <397026784@qq.com>
2020-02-18 23:10:12 +0000

    chapter10

--- rango/views.py
+++ rango/views.py
@@ -14,21 +14,31 @@ from django.shortcuts import redirect
 from django.contrib.auth import logout
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-
+from datetime import datetime
 
 def index(request):
 
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
+
+    visitor_cookie_handler(request)
+
+
     return render(request, 'rango/index.html', context=context_dict)
+
 def about(request):
-    
-    return render(request,'rango/about.html',{})
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
 
@@ -74,7 +84,7 @@ def add_page(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
     except:
         category = None
-    
+
 
     if category is None:
         return redirect(reverse('rango:index'))
@@ -93,7 +103,7 @@ def add_page(request, category_name_slug):
 
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
-            print(form.errors)      
+            print(form.errors)
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
@@ -162,7 +172,7 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rango/login.html')
-    
+
 @login_required
 def restricted(request):
     return render(request, 'rango/restricted.html')
@@ -173,3 +183,27 @@ def user_logout(request):
     logout(request)
 
     return redirect(reverse('rango:index'))
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+    'last_visit',
+    str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+    '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+
+        request.session['last_visit'] = last_visit_cookie
+
+    request.session['visits'] = visits
